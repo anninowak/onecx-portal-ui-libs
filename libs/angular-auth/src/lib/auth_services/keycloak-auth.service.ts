573fffbf96700224ef90e88c0ce5e2d0c31ce2f3
kim.tran <kim.tran@capgemini.com>
2025-02-13 13:46:18 +0100

    feat: init keycloak angular 19 migration

--- libs/angular-auth/src/lib/auth_services/keycloak-auth.service.ts
+++ libs/angular-auth/src/lib/auth_services/keycloak-auth.service.ts
@@ -1,8 +1,10 @@
-import { Injectable, inject } from '@angular/core'
+import { Injectable, effect, inject } from '@angular/core'
 import { ConfigurationService, CONFIG_KEY } from '@onecx/angular-integration-interface'
-import { KeycloakEventType, KeycloakOptions, KeycloakService } from 'keycloak-angular'
-import { KeycloakConfig } from 'keycloak-js'
+import { KEYCLOAK_EVENT_SIGNAL, KeycloakEventType, KeycloakOptions, KeycloakService } from 'keycloak-angular'
+import { KeycloakConfig, KeycloakInitOptions } from 'keycloak-js'
 import { AuthService } from '../auth.service'
+import Keycloak from 'keycloak-js'
+
 
 const KC_REFRESH_TOKEN_LS = 'onecx_kc_refreshToken'
 const KC_ID_TOKEN_LS = 'onecx_kc_idToken'
@@ -10,7 +12,8 @@ const KC_TOKEN_LS = 'onecx_kc_token'
 
 @Injectable()
 export class KeycloakAuthService implements AuthService {
-  private keycloakService = inject(KeycloakService);
+  private keycloakService = inject(Keycloak);
+  private readonly keycloakSignal = inject(KEYCLOAK_EVENT_SIGNAL)
   private configService = inject(ConfigurationService);
 
 
@@ -19,7 +22,35 @@ export class KeycloakAuthService implements AuthService {
   /** Inserted by Angular inject() migration for backwards compatibility */
   constructor(...args: unknown[]);
 
-  constructor() {}
+  constructor() {
+    effect(() => {
+      const keycloakEvent = this.keycloakSignal()
+
+      if (keycloakEvent.type === KeycloakEventType.AuthSuccess) {
+        if (this.keycloakService.token) {
+          localStorage.setItem(KC_TOKEN_LS, this.keycloakService.token)
+        } else {
+          localStorage.removeItem(KC_TOKEN_LS)
+        }
+        if (this.keycloakService.idToken) {
+          localStorage.setItem(KC_ID_TOKEN_LS, this.keycloakService.idToken)
+        } else {
+          localStorage.removeItem(KC_ID_TOKEN_LS)
+        }
+        if (this.keycloakService.refreshToken) {
+          localStorage.setItem(KC_REFRESH_TOKEN_LS, this.keycloakService.refreshToken)
+        } else {
+          localStorage.removeItem(KC_REFRESH_TOKEN_LS)
+        }
+      }
+
+      if (keycloakEvent.type === KeycloakEventType.AuthLogout) {
+        console.log('SSO logout nav to root')
+        this.clearKCStateFromLocalstorage()
+        this.keycloakService.login()
+      }
+    })
+  }
 
   public async init(config?: Record<string, unknown>): Promise<boolean> {
     console.time('KeycloakAuthService')
@@ -46,19 +77,19 @@ export class KeycloakAuthService implements AuthService {
 
     const enableSilentSSOCheck = this.configService.getProperty(CONFIG_KEY.KEYCLOAK_ENABLE_SILENT_SSO) === 'true'
 
-    const kcOptions: KeycloakOptions = {
-      loadUserProfileAtStartUp: false,
-      config: kcConfig,
-      initOptions: {
+    const kcOptions: KeycloakInitOptions = {
+      // loadUserProfileAtStartUp: false,
+      // config: kcConfig,
+      // initOptions: {
         onLoad: 'check-sso',
         checkLoginIframe: false,
         silentCheckSsoRedirectUri: enableSilentSSOCheck ? this.getSilentSSOUrl() : undefined,
         idToken: idToken || undefined,
         refreshToken: refreshToken || undefined,
         token: token || undefined,
-      },
-      enableBearerInterceptor: false,
-      bearerExcludedUrls: ['/assets'],
+      // },
+      // enableBearerInterceptor: false,
+      // bearerExcludedUrls: ['/assets'],
     }
 
     return this.keycloakService
@@ -69,7 +100,7 @@ export class KeycloakAuthService implements AuthService {
       })
       .then((loginOk) => {
         if (loginOk) {
-          return this.keycloakService.getToken()
+          return this.keycloakService.token
         } else {
           return this.keycloakService.login(config).then(() => 'login')
         }
@@ -101,31 +132,31 @@ export class KeycloakAuthService implements AuthService {
   }
 
   private setupEventListener() {
-    this.keycloakService.keycloakEvents$.subscribe((ke) => {
-      if (this.keycloakService.getKeycloakInstance().token) {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        localStorage.setItem(KC_TOKEN_LS, this.keycloakService.getKeycloakInstance().token!)
-      } else {
-        localStorage.removeItem(KC_TOKEN_LS)
-      }
-      if (this.keycloakService.getKeycloakInstance().idToken) {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        localStorage.setItem(KC_ID_TOKEN_LS, this.keycloakService.getKeycloakInstance().idToken!)
-      } else {
-        localStorage.removeItem(KC_ID_TOKEN_LS)
-      }
-      if (this.keycloakService.getKeycloakInstance().refreshToken) {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        localStorage.setItem(KC_REFRESH_TOKEN_LS, this.keycloakService.getKeycloakInstance().refreshToken!)
-      } else {
-        localStorage.removeItem(KC_REFRESH_TOKEN_LS)
-      }
-      if (ke.type === KeycloakEventType.OnAuthLogout) {
-        console.log('SSO logout nav to root')
-        this.clearKCStateFromLocalstorage()
-        this.keycloakService.login(this.kcConfig)
-      }
-    })
+    // this.keycloakService.keycloakEvents$.subscribe((ke) => {
+    //   if (this.keycloakService.getKeycloakInstance().token) {
+    //     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    //     localStorage.setItem(KC_TOKEN_LS, this.keycloakService.getKeycloakInstance().token!)
+    //   } else {
+    //     localStorage.removeItem(KC_TOKEN_LS)
+    //   }
+    //   if (this.keycloakService.getKeycloakInstance().idToken) {
+    //     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    //     localStorage.setItem(KC_ID_TOKEN_LS, this.keycloakService.getKeycloakInstance().idToken!)
+    //   } else {
+    //     localStorage.removeItem(KC_ID_TOKEN_LS)
+    //   }
+    //   if (this.keycloakService.getKeycloakInstance().refreshToken) {
+    //     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    //     localStorage.setItem(KC_REFRESH_TOKEN_LS, this.keycloakService.getKeycloakInstance().refreshToken!)
+    //   } else {
+    //     localStorage.removeItem(KC_REFRESH_TOKEN_LS)
+    //   }
+    //   if (ke.type === KeycloakEventType.OnAuthLogout) {
+    //     console.log('SSO logout nav to root')
+    //     this.clearKCStateFromLocalstorage()
+    //     this.keycloakService.login(this.kcConfig)
+    //   }
+    // })
   }
 
   private clearKCStateFromLocalstorage() {
@@ -143,10 +174,10 @@ export class KeycloakAuthService implements AuthService {
   }
 
   getIdToken(): string | null {
-    return this.keycloakService.getKeycloakInstance().idToken ?? null
+    return this.keycloakService.idToken ?? null
   }
   getAccessToken(): string | null {
-    return this.keycloakService.getKeycloakInstance().token ?? null
+    return this.keycloakService.token ?? null
   }
 
   logout(): void {
@@ -154,7 +185,7 @@ export class KeycloakAuthService implements AuthService {
   }
 
   async updateTokenIfNeeded(): Promise<boolean> {
-    if (!(await this.keycloakService.isLoggedIn())) {
+    if (!(this.keycloakService.authenticated)) {
       return this.keycloakService.login(this.kcConfig).then(() => false)
     } else {
       return this.keycloakService.updateToken()
